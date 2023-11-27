e28378a2c04adcf28a5682d62fa7ee9396b4c1a9
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2023-11-27 16:04:40 +0100

    Additions of 3 Products in the DB

--- src/main/java/com/dgmf/Application.java
+++ src/main/java/com/dgmf/Application.java
@@ -1,11 +1,19 @@
 package com.dgmf;
 
+import com.dgmf.entity.Product;
+import com.dgmf.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
+
 @SpringBootApplication
 public class Application implements CommandLineRunner {
+	@Autowired
+	private ProductRepository productRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
@@ -13,6 +21,26 @@ public class Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		Product product1 = Product.builder()
+				.name("Computer")
+				.price(4300L)
+				.quantity(3)
+				.build();
 
+		Product product2 = Product.builder()
+				.name("Printer")
+				.price(1200L)
+				.quantity(18)
+				.build();
+
+		Product product3 = Product.builder()
+				.name("Smart Phone")
+				.price(1200L)
+				.quantity(32)
+				.build();
+
+		List<Product> products = productRepository.saveAll(
+				Arrays.asList(product1, product2, product3)
+		);
 	}
 }
